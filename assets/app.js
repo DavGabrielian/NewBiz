11e0b868d14670f3f8b6a4618f4222e6108c224f
David Gabrielian <davidgabrielofficial@gmail.com>
2020-08-31 21:52:13 +0400

    final

--- assets/app.js
+++ assets/app.js
@@ -2,17 +2,9 @@ let menu = document.getElementById("menu");
 let close = document.getElementById("close");
 
 openMenu = () => {
-  if ((menu.style.display = "block")) {
-    (document.getElementById("list").style.display = "block") &&
-      (document.getElementById("menu").style.display = "none") &&
-      (document.getElementById("close").style.display = "block");
-  }
+  document.getElementById("list").style.width = "250px";
 };
 
 closeMenu = () => {
-  if ((close.style.display = "block")) {
-    (document.getElementById("list").style.display = "none") &&
-      (document.getElementById("menu").style.display = "block") &&
-      (document.getElementById("close").style.display = "none");
-  }
+  document.getElementById("list").style.width = "0";
 };
